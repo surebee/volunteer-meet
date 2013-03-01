0c00af7ddc76c886cb301ee5489ef1255def92aa
Suresh Chandrasekharan <sukuchand@gmail.com>
2013-03-01 14:32:10 +0530

    First commit for Volunteer meeting app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VolunteerMeet::Application.config.secret_token = '672606f5e6136fa8bc5bf7c9e726a7d2eca7fe047be9c773134568f6fe3000a76a845167c35113b193b63b457a74f684ce8de96d5eb2759b6909c3dd3e9d6949'
